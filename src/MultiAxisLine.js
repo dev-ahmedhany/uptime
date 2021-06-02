12f82a894e6ba9aeeab4352a02ce4017a9ccf63f
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2021-06-02 23:22:34 +0200

    fix bugs

--- src/MultiAxisLine.js
+++ src/MultiAxisLine.js
@@ -11,7 +11,7 @@ const getAvgFunction = function (chunkSize, dataSource) {
     let i, j, temparray, chunk = chunkSize;
     const list = Object.entries(data);
 
-    for (i = 0, j = Object.keys(data).length; i < j; i += chunk) {///// i= 1 !!! to ignore 00:00 result
+    for (i = 0, j = Object.keys(data).length; i < j; i += chunk) {
         temparray = list.slice(i, i + chunk);
         // do whatever
 
@@ -31,7 +31,8 @@ const getAvgFunction = function (chunkSize, dataSource) {
     for (const timeStamp in data) {
         const time = Number(timeStamp) * timeInterval;
         for (let j = 0; j < result.length; j++) {
-            result[j].data.push({ primary: time, secondary: data[timeStamp][j] });
+            if (data[timeStamp][j])
+                result[j].data.push({ primary: time, secondary: data[timeStamp][j] });
         }
     }
 
@@ -49,15 +50,14 @@ export default function Line() {
     const [chunkSize, setChunkSize] = React.useState(10);
 
 
-    
     useEffect(() => {
         const fetchData = async () => {
             const res = await fetch('/API')
             return res.ok && await res.json()
         }
         fetchData().then(res => {
-            if(!res) return;
-            
+            if (!res) return;
+
             setDataSource(JSON.parse(JSON.stringify(res)));
 
             let { result, timeInterval, data } = res;
@@ -65,14 +65,14 @@ export default function Line() {
             for (const timeStamp in data) {
                 const time = Number(timeStamp) * timeInterval;
                 for (let j = 0; j < result.length; j++) {
-                    result[j].data.push({ primary: time, secondary: data[timeStamp][j] });
+                    if (data[timeStamp][j])
+                        result[j].data.push({ primary: time, secondary: data[timeStamp][j] });
                 }
             }
 
             setData(result);
             document.getElementById("resizable").style = `position: relative; user-select: auto;
                 width: 91vw; height: 45vw; box-sizing: border-box; flex-shrink: 0;`;
-            
         });
 
     }, []);
@@ -85,11 +85,11 @@ export default function Line() {
                 const item = element.data[j];
                 if (item.secondary > 100000) {
                     const date = new Date(item.primary);
-                    items.push(<li key={j}>
+                    items.push(<li key={i + "n" + j}>
                         {`${element.label}\t${date.toLocaleDateString("en-US")}
                         \t${date.toLocaleTimeString("en-US")}
                         \tError Code:${parseInt((100000 - Number(item.secondary)) * 1000)}`}
-                        </li>)
+                    </li>)
                 }
             }
         }
