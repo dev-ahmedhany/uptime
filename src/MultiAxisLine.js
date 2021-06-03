5d803f320db040a43ad3ba8f8935f85a4b47445c
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2021-06-03 16:30:30 +0200

    fix performance

--- src/MultiAxisLine.js
+++ src/MultiAxisLine.js
@@ -1,45 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { Chart } from 'react-charts'
 import { Resizable } from "re-resizable";
 import Slider from '@material-ui/core/Slider';
 import Details from './components/Details'
 import { CircularProgress, Typography } from '@material-ui/core';
-
-
-const getAvgFunction = function (chunkSize, result, timeInterval, data) {
-    let finalResult = JSON.parse(JSON.stringify(result))
-    let newValue = [];
-    let i, j, temparray, chunk = chunkSize;
-    const list = Object.entries(data);
-
-    for (i = 0, j = Object.keys(data).length; i < j; i += chunk) {
-        temparray = list.slice(i, i + chunk);
-        // do whatever
-
-        let averageDate = (Number(temparray[temparray.length - 1][0]) + Number(temparray[0][0])) / 2 + 0.5;
-        averageDate = averageDate % 1 === 0.5 ? averageDate : averageDate + 0.5;
-        averageDate = (chunk % 2 === 0) ? averageDate - 0.5 : averageDate;
-
-        let averageArray = [];
-        for (let k = 0; k < result.length; k++) {
-            const average = temparray.reduce((prev, crnt) => prev + Number(crnt[1][k]), 0) / temparray.length;
-            averageArray.push(average);
-        }
-        newValue[averageDate] = averageArray;
-    }
-    data = newValue;
-
-    for (const timeStamp in data) {
-        const time = Number(timeStamp) * timeInterval;
-        for (let j = 0; j < result.length; j++) {
-            if (data[timeStamp][j])
-                finalResult[j].data.push({ primary: time, secondary: data[timeStamp][j] });
-        }
-    }
-
-    return finalResult;
-}
-
 
 export default function Line() {
     // series array
@@ -49,7 +13,6 @@ export default function Line() {
     const [dataAvg, setDataAvg] = useState();
     const [info, setInfo] = useState([]);
     const [chunkSize, setChunkSize] = React.useState(10);
-    console.log("rendered");
 
     useEffect(() => {
         const fetchData = async () => {
@@ -58,56 +21,91 @@ export default function Line() {
         }
         fetchData().then(res => {
             if (!res) return;
-
             setDataSource(res);
-
-            const { result, timeInterval, data } = res;
-            let finalResult = JSON.parse(JSON.stringify(result))
-            let errorList = [];
-
-            for (const timeStamp in data) {
-                const time = Number(timeStamp) * timeInterval;
-                for (let i = 0; i < result.length; i++) {
-
-                    errorList.push({ name: result[i].label, Data: [] })
-
-                    if (data[timeStamp][i]) {
-
-                        finalResult[i].data.push({ primary: time, secondary: data[timeStamp][i] });
-
-                        if (data[timeStamp][i] > 100000) {
-                            errorList[i].Data.push({
-                                time: (new Date(time)).toLocaleTimeString("en-US", { hour: '2-digit', minute: '2-digit' }),
-                                code: parseInt(Math.abs((100000 - Number(data[timeStamp][i])) * 1000))
-                            })
-                        }
-                    }
-                }
-            }
-
-            setData(finalResult);
-            setInfo(errorList);
-
         });
-
     }, []);
 
     const handleChange = (event, newValue) => {
         setChunkSize(newValue);
     };
 
+    const getAvgFunction = useCallback((chunkSize, result, timeInterval, data) => {
+        let finalResult = JSON.parse(JSON.stringify(result))
+        let newValue = [];
+        let i, j, temparray, chunk = chunkSize;
+        const list = Object.entries(data);
+
+        for (i = 0, j = Object.keys(data).length; i < j; i += chunk) {
+            temparray = list.slice(i, i + chunk);
+            // do whatever
+
+            let averageDate = (Number(temparray[temparray.length - 1][0]) + Number(temparray[0][0])) / 2 + 0.5;
+            averageDate = averageDate % 1 === 0.5 ? averageDate : averageDate + 0.5;
+            averageDate = (chunk % 2 === 0) ? averageDate - 0.5 : averageDate;
+
+            let averageArray = [];
+            for (let k = 0; k < result.length; k++) {
+                const average = temparray.reduce((prev, crnt) => prev + Number(crnt[1][k]), 0) / temparray.length;
+                averageArray.push(average);
+            }
+            newValue[averageDate] = averageArray;
+        }
+        data = newValue;
+
+        for (const timeStamp in data) {
+            const time = Number(timeStamp) * timeInterval;
+            for (let j = 0; j < result.length; j++) {
+                if (data[timeStamp][j])
+                    finalResult[j].data.push({ primary: time, secondary: data[timeStamp][j] });
+            }
+        }
+
+        return finalResult;
+    }, []);
+
     useEffect(() => {
         if (!data) return;
         setIsDataAvalible(true);
-        console.log("data ready", new Date());
     }, [data]);
 
     useEffect(() => {
         if (!dataSource) return;
-        if (!getAvgFunction) return;
+
+        const { result, timeInterval, data } = dataSource;
+        let finalResult = JSON.parse(JSON.stringify(result))
+        let errorList = [];
+        for (let j = 0; j < result.length; j++) {
+            errorList.push({ name: result[j].label, Data: [] })
+        }
+
+        for (const timeStamp in data) {
+            const time = Number(timeStamp) * timeInterval;
+            for (let i = 0; i < result.length; i++) {
+
+                if (data[timeStamp][i]) {
+
+                    finalResult[i].data.push({ primary: time, secondary: data[timeStamp][i] });
+
+                    if (data[timeStamp][i] > 100000) {
+                        errorList[i].Data.push({
+                            time: (new Date(time)).toLocaleTimeString("en-US", { hour: '2-digit', minute: '2-digit' }),
+                            code: parseInt(Math.abs((100000 - Number(data[timeStamp][i])) * 1000))
+                        })
+                    }
+                }
+            }
+        }
+
+        setData(finalResult);
+        setInfo(errorList);
+
+    }, [dataSource]);
+
+    useEffect(() => {
+        if (!dataSource) return;
         const { result, timeInterval, data } = dataSource;
         setDataAvg(getAvgFunction(chunkSize, result, timeInterval, data));
-    }, [chunkSize, dataSource]);
+    }, [chunkSize, dataSource, getAvgFunction]);
 
     const series = React.useMemo(() => ({ showPoints: false }), [])
     const axes = React.useMemo(() => [{ primary: true, type: 'time', position: 'bottom' },
